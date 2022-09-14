51e34685784ff1749cf0fbae33666bde8aff7d29
SajawalFareedi <sajawalfareedi448@gmail.com>
2022-09-14 19:02:35 +0500

    init

---
+++ enable-mint-ui/src/App.js
@@ -0,0 +1,22 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import * as Pages from "./pages";
+import { SignUpFlowHandler } from "./utils/routesHelper"
+import { GetTabSettings } from "./utils/tabsHandler";
+
+export const App = ({ root }) => {
+  GetTabSettings()
+
+  return (
+    <Routes>
+      <Route path="/" element={<Pages.LandingPage />} />
+      <Route path="/about-us" element={<Pages.AboutUsPage />} />
+      <Route path="/pricing" element={<Pages.Pricing />} />
+      <Route path="/login" element={<Pages.LoginPage />} />
+      <Route path="/reset-password" element={<Pages.ForgetPassword />} />
+      <Route path="/signup" element={<SignUpFlowHandler /> } />
+      <Route path="/help-center" element={<Pages.HelpCenter />} />
+
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
